ef0b5dd2df8b88f6a91e3e381fded978e98cd3d3
juratali <juratbekxudayberganov9@gmail.com>
2024-04-30 10:01:32 +0500

    api url is chanegd

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://api.urganch-ttb.uz/api",
-  baseURL: "http://localhost:8080/api",
+    baseURL: "https://api.urganch-ttb.uz/api",
+    // baseURL: "http://localhost:8080/api",
 });
 
 export default api;
