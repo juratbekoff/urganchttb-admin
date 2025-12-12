317c7ea40e52760d9d24b80c18618e8f3913fe64
Juratbek <104249426+juratbekoff@users.noreply.github.com>
2025-12-12 11:55:14 +0500

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.urganch-ttb.uz/api",
+    baseURL: import.meta.env.VITE_API_URL!,
     // baseURL: "http://localhost:8080/api",
 });
 
